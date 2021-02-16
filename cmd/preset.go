13a950d8a00a226eff63a56c0fd8a418841551ea
Wesley Mutwiri <wesleymochiemo@gmail.com>
2021-02-16 16:52:54 +0300

    Updated svelte-snowpack script

--- cmd/preset.go
+++ cmd/preset.go
@@ -1,7 +1,11 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
+	"io"
+	"log"
+	"os"
 	"os/exec"
 	"strings"
 
@@ -13,16 +17,23 @@ var presetCmd = &cobra.Command{
 	Use:   "create",
 	Short: "Select a simple preset to install svelte template",
 	RunE: func(cmd *cobra.Command, args []string) error {
-		if err := getPresets(); err != nil {
-			return err
+		if len(args) < 1 {
+			if err := getPresets("svelte-template"); err != nil {
+				return err
+			}
+		} else {
+			folderName := args[0]
+			if err := getPresets(folderName); err != nil {
+				return err
+			}
 		}
 		return nil
 	},
 }
 
-func getPresets() error {
+func getPresets(folder string) error {
 	items := []presets{
-		{Name: "svelte-rollup", Url: "https://svelte.dev"},
+		{Name: "svelte-rollup", Url: "https://github.com/sveltejs/template.git"},
 		{Name: "svelte-snowpack", Url: "https://svelte.dev"},
 	}
 	templates := &promptui.SelectTemplates{
@@ -57,18 +68,23 @@ func getPresets() error {
 	switch items[i].Name {
 	case "svelte-rollup":
 		fmt.Println("Installing the rollup template")
-		clone := exec.Command("git", "clone", "--depth", "1", "https://github.com/sveltejs/template.git", "svelty-trial")
+		clone := exec.Command("git", "clone", "--depth", "1", items[i].Url, folder)
 		clone.Run()
 		removeGit := exec.Command("rm", "-rf", "/svelty-trial/.git")
 		removeGit.Run()
 		fmt.Println("Installation done. Kindly change directory")
+
 	case "svelte-snowpack":
 		fmt.Println("Installing the Snowpack template")
-		clone := exec.Command("git", "clone", "--depth", "1", "https://github.com/sveltejs/template.git", "svelty-trial")
-		clone.Run()
-		removeGit := exec.Command("rm", "-rf", "/svelty-trial/.git")
-		removeGit.Run()
-		fmt.Println("Installation done. Kindly change directory")
+		clone := exec.Command("npx", "create-snowpack-app", folder, "--template", "@snowpack/app-template-svelte")
+		var stdoutBuf, stderrBuf bytes.Buffer
+		clone.Stdout = io.MultiWriter(os.Stdout, &stdoutBuf)
+		clone.Stderr = io.MultiWriter(os.Stderr, &stderrBuf)
+
+		err := clone.Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	// clone := exec.Command("git", "clone", "git@github.com:wesleymutwiri/svelte-ecommerce-design.git")
